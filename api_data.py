fb4a7334f8c461dc9d26654c8d7c2c7f4845c6e1
JinhuaSu <944866518@qq.com>
2021-01-24 15:32:22 +0000

    [mod] modify api data for calculate default com

--- api_data.py
+++ api_data.py
@@ -15,6 +15,10 @@ pro = ts.pro_api(token)
 def get_real_data(code, start_date, end_date):
     global pro
     df = pro.balancesheet(ts_code=code, start_date=start_date, end_date=end_date)
+    # profit_to_op
+    df3 = pro.fina_indicator(ts_code=code, start_date=start_date, end_date=end_date)
+    print(df3["profit_to_op"])
+    print(df3["end_date"])
 
     def find_total_liab(date_str, df):
         date_month = int(date_str[:6])
@@ -29,6 +33,16 @@ def get_real_data(code, start_date, end_date):
             df.loc[i, "total_hldr_eqy_inc_min_int"],
         )
 
+    def find_profit_to_op(date_str, df):
+        date_month = int(date_str[:6])
+        for i in range(len(df)):
+            end_date = df.loc[i, "end_date"]
+            months_delta = int(end_date[:6]) - date_month
+            # print(int(end_date[:6]), date_month, months_delta)
+            if months_delta < 3 and months_delta >= 0:
+                break
+        return df.loc[i, "profit_to_op"]
+
     code = code.split(".")[1].lower() + code.split(".")[0]
     # df2 = pro.daily(ts_code=code, start_date=start_date, end_date=end_date)
     df2 = ak.stock_zh_a_daily(
@@ -38,21 +52,24 @@ def get_real_data(code, start_date, end_date):
     df2["total_liab"] = None
     df2["total_share"] = None
     df2["total_hldr_eqy_inc_min_int"] = None
+    df2["profit_to_op"] = None
 
-    for date_str in df2.index:
+    for i, date_str in enumerate(df2.index):
+        input_str = "20" + date_str.strftime("%y%m%d")
+        # print(input_str)
         (
-            df2["total_liab"],
-            df2["total_share"],
-            df2["total_hldr_eqy_inc_min_int"],
-        ) = find_total_liab(date_str.strftime("%y%m%d"), df)
-
+            df2.loc[date_str, "total_liab"],
+            df2.loc[date_str, "total_share"],
+            df2.loc[date_str, "total_hldr_eqy_inc_min_int"],
+        ) = find_total_liab(input_str, df)
+        df2.loc[date_str, "profit_to_op"] = find_profit_to_op(input_str, df3)
     return df2
 
 
 if __name__ == "__main__":
     code = "601398.SH"
     df = get_real_data(code, "20180101", "20201210")
-    df.to_csv('%s.csv'%code)
+    df.to_csv("%s.csv" % code)
     print(df)
 
 
